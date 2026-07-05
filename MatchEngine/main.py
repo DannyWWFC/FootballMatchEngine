7d3f565c38ee93f372357375315cb95489a66ab2
DannyWWFC <dannych71@gmail.com>
2026-07-05 16:28:47 +0100

    Added relegation counts to the output

--- MatchEngine/main.py
+++ MatchEngine/main.py
@@ -7,7 +7,7 @@ from table import Table
 
 engine = MatchEngine()
 
-with open("leagues/championship.json", "r") as f:
+with open("leagues/premier_league.json", "r") as f:
     team_data = json.load(f)
 
 teams = [
@@ -17,13 +17,24 @@ teams = [
 
 tables = []
 title_wins = defaultdict(int)
+relegations = defaultdict(int)
 
-for i in range (0, 100):
+relegation_spots = 3
+
+for i in range (0, 1000):
     table, results = engine.simulate_season(teams)
     tables.append(table)
 
     winner = max(table.values(), key=lambda entry: entry.points)
     title_wins[winner.team.name] += 1
+
+    bottom_teams = sorted(
+        table.values(),
+        key=lambda entry: entry.points
+    )[:relegation_spots]
+
+    for team in bottom_teams:
+        relegations[team.team.name] += 1
 
 highest_points = 0
 
@@ -45,3 +56,6 @@ for team, wins in sorted(title_wins.items(), key=lambda x: x[1], reverse=True):
 
 print(f"\nHighest points tally in a season: {highest_points}");
 
+print("\n=== RELEGATION COUNTS ===")
+for team, times in sorted(relegations.items(), key=lambda x: x[1], reverse=True):
+    print(f"{team}: {times}")
